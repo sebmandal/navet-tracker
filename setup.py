0fadbb82b8f030d49ac9e064ab59b546abe9c7b1
Sebastian Mandal <sebastian@mandal.group>
2024-10-04 04:12:34 +0200

    chore: update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name="navet-tracker",
     version="0.0",
-    description="A simple script that tracks the number of open positions for events on ifinavet.no and notifies a GitHub repository of the number of open positions",
+    description="A simple script that tracks the number of open positions for events on ifinavet.no and notifies a Discord webhook of the number of open positions",
     author="Sebastian Mandal",
     author_email="sebastian@mandal.group",
     packages=[""],
