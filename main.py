13283050102fe265decf1cf367fcd78c75d01760
Sebastian Mandal <sebastian@mandal.group>
2024-10-04 02:38:25 +0200

    feat: adds scheduler module

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from navet_tracker.core import DataSucker
 from navet_tracker.notifications import Notification
+from navet_tracker.scheduler import Scheduler
 
 
-def main():
+def run():
     m = DataSucker()  # Zakaria suggested this name and I think it's hilarious
     url = m.next_event()
     p = m.get_participants(url)
@@ -12,8 +13,15 @@ def main():
         print("No open positions")
         return
 
-    n = Notification()
-    n.notify(b, p)
+    print(f"Open positions at {b}: {p}")
+    # n = Notification()
+    # n.notify(b, p)
+
+
+def main():
+    per_day = 4
+    s = Scheduler(run, (24 / per_day) * 60 * 60)  # 6 hours / 4 times a day
+    s.start()
 
 
 if __name__ == "__main__":
