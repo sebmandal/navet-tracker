9e7b896f6f416e1702fdc8c70d410ba43818adf3
Sebastian Mandal <sebastian@mandal.group>
2024-10-04 21:34:29 +0200

    feat(notify): multiple webhooks functionality

--- src/navet_tracker/notifications/notify.py
+++ src/navet_tracker/notifications/notify.py
@@ -6,9 +6,10 @@ from navet_tracker.notifications.discord import send_discord_message
 class Notification:
     def __init__(self):
         load_dotenv()
-        self.webhook_url = os.getenv("DISCORD_WEBHOOK_URL")
+        self.webhook_urls = os.getenv("DISCORD_WEBHOOK_URLS").split(",")
 
     def notify(self, message: str) -> None:
-        if not self.webhook_url:
-            raise ValueError("Discord webhook URL is not set in the .env file.")
-        send_discord_message(self.webhook_url, message)
+        if not self.webhook_urls:
+            raise ValueError("Discord webhook URLs are not set in the .env file.")
+        for url in self.webhook_urls:
+            send_discord_message(url.strip(), message)
